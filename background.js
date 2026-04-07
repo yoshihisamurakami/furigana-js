4139e597face7e7536aa8b5762f9a69d757d67f9
muranet+git@gmail.com <muranet+git@gmail.com>
2026-04-07 18:51:30 +0900

    background.jsを利用するよう修正

--- background.js
+++ background.js
@@ -1,49 +1,12 @@
 
 const FuriganaApiUrl = 'https://go-furigana-api-72715150088.asia-northeast1.run.app/furigana'
-const CACHE_PREFIX = 'fg_cache_'
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.msg === 'fetch-furigana') {
-        handleFuriganaRequest(request.textList).then(sendResponse)
+        callFuriganaApi(request.textList).then(sendResponse)
         return true // 非同期レスポンスのためチャネルを開いたままにする
     }
 })
-
-const handleFuriganaRequest = async (textList) => {
-    const cacheKeys = textList.map(text => CACHE_PREFIX + text)
-    const cached = await chrome.storage.local.get(cacheKeys)
-
-    const results = new Array(textList.length).fill(undefined)
-    const uncachedIndices = []
-    const uncachedTexts = []
-
-    for (let i = 0; i < textList.length; i++) {
-        const cachedValue = cached[cacheKeys[i]]
-        if (cachedValue !== undefined) {
-            results[i] = cachedValue
-        } else {
-            uncachedIndices.push(i)
-            uncachedTexts.push(textList[i])
-        }
-    }
-
-    if (uncachedTexts.length > 0) {
-        const apiResponse = await callFuriganaApi(uncachedTexts)
-        const newCache = {}
-        for (let j = 0; j < uncachedIndices.length; j++) {
-            const originalIndex = uncachedIndices[j]
-            if (apiResponse[j] !== undefined) {
-                results[originalIndex] = apiResponse[j]
-                newCache[cacheKeys[originalIndex]] = apiResponse[j]
-            }
-        }
-        if (Object.keys(newCache).length > 0) {
-            await chrome.storage.local.set(newCache)
-        }
-    }
-
-    return results
-}
 
 const callFuriganaApi = async (textList) => {
     const requestOptions = {
